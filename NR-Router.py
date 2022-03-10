1581acc74eb3aa208bc08d358738f586538dfc23
108061250 <sam54537@gmail.com>
2022-03-10 14:10:35 +0800

    convert dxf string to svg string

--- NR-Router.py
+++ NR-Router.py
@@ -3,6 +3,12 @@ import ezdxf
 import time
 import base64
 
+# for converting dxf string to svg sting
+from io import StringIO
+import matplotlib.pyplot as plt
+from ezdxf.addons.drawing import RenderContext, Frontend
+from ezdxf.addons.drawing.matplotlib import MatplotlibBackend
+
 from chip import Chip
 from grid import Grid, GridType
 from draw import Draw
@@ -39,7 +45,6 @@ try:
 except:
     ewd_input = None
 
-
 # real ship size
 regular_line_width = int(electrode_size / 10)
 if regular_line_width > MAX_WIRE_WIDTH:
@@ -57,7 +62,6 @@ for unit in UNIT_LIST:
 
 if (regular_line_width + 5) * 1.414 > unit:
     regular_line_width = (int((unit / 1.414) - 5) // 50) * 50
-
 """
     contact section
     - x: 0 ~ (32 * contact_pad_unit)
@@ -72,7 +76,8 @@ top_start_point = [0, 0]
 mid_start_point = [-1630, 11258]
 down_start_point = [0, 56896]
 
-top_section = ChipSection(top_start_point, contactpad_unit * 31, contactpad_unit * 3, contactpad_unit, contactpad_radius)
+top_section = ChipSection(top_start_point, contactpad_unit * 31, contactpad_unit * 3, contactpad_unit,
+                          contactpad_radius)
 top_section.init_grid(GridType.CONTACTPAD)
 top_section.init_tile()
 top_section.init_hub((mid_start_point[1] + top_section.unit * 3 + top_section.redius) // 2)
@@ -80,7 +85,8 @@ top_section.init_hub((mid_start_point[1] + top_section.unit * 3 + top_section.re
 mid_section = ChipSection(mid_start_point, 82000, 42000, tile_unit, contactpad_radius)
 mid_section.init_grid()
 
-down_section = ChipSection(down_start_point,  contactpad_unit * 31, contactpad_unit * 3, contactpad_unit, contactpad_radius)
+down_section = ChipSection(down_start_point, contactpad_unit * 31, contactpad_unit * 3, contactpad_unit,
+                           contactpad_radius)
 down_section.init_grid(GridType.CONTACTPAD)
 down_section.init_tile()
 down_section.init_hub((down_start_point[1] - down_section.redius + mid_start_point[1] + mid_section.height) // 2)
@@ -89,10 +95,11 @@ c_time = time.time()
 
 # read ewd file
 # if ewd_input is None, then open local file
-_chip = Chip('test0307_2_real_chip.ewd', ewd_input)
+_chip = Chip('ewod-chip-project.ewd', ewd_input)
 _chip.setup()
 
-_pseudo_node = PseudoNode(mid_section.grid, _chip.electrode_shape_library, mid_section.start_point, mid_section.unit, _chip.electrode_list)
+_pseudo_node = PseudoNode(mid_section.grid, _chip.electrode_shape_library, mid_section.start_point,
+                          mid_section.unit, _chip.electrode_list)
 _model_mesh = ModelMesh(top_section, mid_section, down_section, _pseudo_node)
 _model_mesh.get_pseudo_node()
 _model_mesh.create_pseudo_node_connection()
@@ -140,7 +147,6 @@ dxf2 = hatch2.paths
 # dxf3 = hatch3.paths
 # dxf4 = hatch4.paths
 
-
 _draw.draw_contact_pad(_chip.contactpad_list, msp)
 _draw.draw_electrodes(_chip.electrode_list, _chip.electrode_shape_library, _model_mesh.electrodes, msp, dxf2)
 
@@ -169,6 +175,16 @@ for electrode in _model_mesh.electrodes:
 
 encode_dxf = doc.encode_base64()
 print(base64.b64decode(encode_dxf).decode())
+
+# dxf string to svg string
+fig = plt.figure()
+ax = fig.add_axes([0, 0, 1, 1])
+ctx = RenderContext(doc)
+out = MatplotlibBackend(ax)
+Frontend(ctx, out).draw_layout(doc.modelspace(), finalize=True)
+svg = StringIO()
+fig.savefig(svg, format='svg')
+print(svg.getvalue())
 
 # doc.saveas('dwg/' + ewd_name + '.dxf')
 
